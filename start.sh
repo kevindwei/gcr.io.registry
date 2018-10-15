255e1e467222e1f60d91f6b9dd8891854725c005
杜炜垣 <865169974@qq.com>
2018-10-15 15:37:52 +0800

    add9

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/software/nginx/nginx -p `pwd` -c /software/nginx/ABTestingGateway/utils/conf/stable.conf
-/software/nginx/nginx -p `pwd` -c /software/nginx/ABTestingGateway/utils/conf/beta1.conf
-/software/nginx/nginx -p `pwd` -c /software/nginx/ABTestingGateway/utils/conf/beta2.conf
-/software/nginx/nginx -p `pwd` -c /software/nginx/ABTestingGateway/utils/conf/beta3.conf
-/software/nginx/nginx -p `pwd` -c /software/nginx/ABTestingGateway/utils/conf/beta4.conf
-/software/nginx/nginx -p `pwd` -c /software/nginx/ABTestingGateway/utils/conf/nginx.conf
+nginx  -c /software/nginx/ABTestingGateway/utils/conf/stable.conf
+nginx  -c /software/nginx/ABTestingGateway/utils/conf/beta1.conf
+nginx  -c /software/nginx/ABTestingGateway/utils/conf/beta2.conf
+nginx  -c /software/nginx/ABTestingGateway/utils/conf/beta3.conf
+nginx  -c /software/nginx/ABTestingGateway/utils/conf/beta4.conf
+nginx  -c /software/nginx/ABTestingGateway/utils/conf/nginx.conf

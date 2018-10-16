ff1dc1b91cf56ed0c8ec41e449f4fc50564e14e5
杜炜垣 <865169974@qq.com>
2018-10-16 17:12:13 +0800

    uuu8

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cat /start.sh
 
 /software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
 /software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/beta1.conf

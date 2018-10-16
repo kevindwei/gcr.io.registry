16cf633b5c05496096b0bed106e9c3c1ce6fba10
杜炜垣 <865169974@qq.com>
2018-10-16 17:09:19 +0800

    uuu7

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 cat /start.sh
 
 /software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
-/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
-/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
-/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
-/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
-/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
+/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/beta1.conf
+/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/beta2.conf
+/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/beta3.conf
+/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/beta4.conf
+/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/nginx.conf
 

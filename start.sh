92685d508deb74a0eb52fe0281a122f6a12da2ac
杜炜垣 <865169974@qq.com>
2018-10-16 16:27:50 +0800

    uuu2

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-/software/nginx/nginx/sbin/nginx  -c /software/nginx/ABTestingGateway/utils/conf/stable.conf
-/software/nginx/nginx/sbin/nginx  -c /software/nginx/ABTestingGateway/utils/conf/beta1.conf
-/software/nginx/nginx/sbin/nginx  -c /software/nginx/ABTestingGateway/utils/conf/beta2.conf
-/software/nginx/nginx/sbin/nginx  -c /software/nginx/ABTestingGateway/utils/conf/beta3.conf
-/software/nginx/nginx/sbin/nginx  -c /software/nginx/ABTestingGateway/utils/conf/beta4.conf
-/software/nginx/nginx/sbin/nginx  -c /software/nginx/ABTestingGateway/utils/conf/nginx.conf
+
+/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
+/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
+/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
+/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
+/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
+/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
+

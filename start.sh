793625adb8e716720524979c5bb8f4b24f961130
杜炜垣 <865169974@qq.com>
2018-10-17 13:50:30 +0800

    aa3

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/stable.conf
-/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/beta1.conf
-/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/beta2.conf
-/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/beta3.conf
-/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/beta4.conf
-/software/ngx_openresty/nginx/sbin/nginx  -c /ABTestingGateway/utils/conf/nginx.conf
+/software/ngx_openresty/nginx/sbin/nginx -p "/ABTestingGateway/utils" -c /ABTestingGateway/utils/conf/stable.conf
+/software/ngx_openresty/nginx/sbin/nginx -p "/ABTestingGateway/utils" -c /ABTestingGateway/utils/conf/beta1.conf
+/software/ngx_openresty/nginx/sbin/nginx -p "/ABTestingGateway/utils" -c /ABTestingGateway/utils/conf/beta2.conf
+/software/ngx_openresty/nginx/sbin/nginx -p "/ABTestingGateway/utils" -c /ABTestingGateway/utils/conf/beta3.conf
+/software/ngx_openresty/nginx/sbin/nginx -p "/ABTestingGateway/utils" -c /ABTestingGateway/utils/conf/beta4.conf
+/software/ngx_openresty/nginx/sbin/nginx -p "/ABTestingGateway/utils" -c /ABTestingGateway/utils/conf/nginx.conf
 
